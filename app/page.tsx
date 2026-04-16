ebf93f331ea1dba353807b3dcb7656aa212cdd6e
gohan12317 <gohanpeter@gmail.com>
2026-04-16 12:08:02 +0800

    correct push

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,6 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <h1>Welcome to PickleSpot!</h1>
-      <p>Your one-stop destination for all things pickles.</p>
-      <Image
-        src="/pickle-image.jpg"
-        alt="A delicious pickle"
-        width={500}
-        height={300}
-      />
     </main>
   );
 }
